86e696adafc24537999cdc2fd5721185808bbec8
Dani Lupión <danilupion@gmail.com>
2022-04-30 19:36:45 +0200

    Deps bump and refactor to use concurrent mode in react

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
 import client from './client/websocket';
 import App from './components/App';
@@ -31,11 +31,11 @@ client({
   },
 });
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
